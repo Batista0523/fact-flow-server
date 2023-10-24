a31f2fe312edd0919b0a7b86b42113f8dfd7f781
elisaul batista <elisaulbatista@pursuit.org>
2023-10-24 19:53:46 -0400

    fix the port

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const cors = require('cors');  
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+require('dotenv').config();
 app.use(express.json());
 
 app.use(cors({
@@ -10,10 +10,10 @@ app.use(cors({
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 }));
 
-
 const transactionsRouter = require('./routes/transactions');
 app.use('/transactions', transactionsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
